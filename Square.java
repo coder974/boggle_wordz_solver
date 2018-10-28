6d93693553811bc68a0b8fb4c14097429865558e
coder974 <44545258+coder974@users.noreply.github.com>
2018-10-28 13:27:38 +0200

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,33 @@
+package com.ylajaaski;
+
+
+/**The game consist of different squares which have their own coordinate and character.
+ * There can only be one of each coordinate, but characters can be repeated.
+ */
+public class Square {
+
+    /**The coordinate of the character**/
+    private Coordinate coordinate;
+
+
+    /**Square has one and only one character which can form words with
+     * other squares.**/
+    private char aChar;
+
+
+    /**Constructor**/
+    Square(Coordinate coordinate, char aChar) {
+        this.coordinate = coordinate;
+        this.aChar = aChar;
+    }
+
+
+    /**Making getters for the private values.**/
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public char getChar() {
+        return aChar;
+    }
+}
